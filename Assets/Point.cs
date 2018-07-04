4286dba9a296e5a1d43e0def284a235dce34d321
ito-hiroki <i.hiroki0307@icloud.com>
2018-07-05 00:19:26 +0900

    点の色が変わるようになった
    最初にlaytracing周りで4つエラーがでるので要bugfix

---
+++ Assets/Point.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Point : MonoBehaviour {
+
+    float x, y;
+
+    // Use this for initialization
+    void Start()
+    { 
+        x = transform.position.x;
+        y = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnUserAction()
+    {
+        Debug.Log("sucess");
+        this.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        
+    }
+}
