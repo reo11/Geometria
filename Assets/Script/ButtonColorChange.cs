0df18c11ce32456ea98297e0f6c6dc7bd86458e9
hir0king <i.hiroki0307@icloud.com>
2018-08-12 16:21:55 +0900

    点をクリックしたら色が変わるように変更

---
+++ Assets/Script/ButtonColorChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonColorChange : MonoBehaviour {
+
+    [SerializeField]
+    Color btnColor = Color.red;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeButtonColor()
+    {
+        var button = this.GetComponent<Button>();
+        var colors = button.colors;
+
+        /*
+        colors.normalColor = btnColor;
+        colors.highlightedColor = btnColor;
+        colors.pressedColor = btnColor;
+        colors.disabledColor = btnColor;
+
+        button.colors = colors;
+        */
+        button.image.color = btnColor;
+    }
+}
