cb1bbc2e998d74e45963230e91027b4b644b9da8
ito-hiroki <i.hiroki0307@icloud.com>
2018-08-19 22:53:05 +0900

    線を引く機能の追加

--- Assets/Script/PointManeger.cs
+++ Assets/Script/PointManeger.cs
@@ -7,6 +7,7 @@ public class PointManeger : MonoBehaviour {
 
     public GameObject pointPrefab;
     public Canvas canvas;
+    public GameObject linePrefab;
     const int pointNum = 5;
 
     // 一時的に位置を用意
@@ -17,6 +18,9 @@ public class PointManeger : MonoBehaviour {
         new Vector3(300f, 0f, 0f),
         new Vector3(0f, 500f, 0f),
         };
+
+    // poslistのworld座標版(poslistはui座標)
+    Vector3[] WorldPosList = new Vector3[pointNum];
 
     // 点のつながりを保存(Falseで初期化される)
     bool[,] connectionMap = new bool[pointNum, pointNum];
@@ -42,7 +46,9 @@ public class PointManeger : MonoBehaviour {
     {
         var prefab = Instantiate(pointPrefab, canvas.transform, false) as GameObject;
         RectTransform rectTransform = prefab.GetComponent<RectTransform>();
+        //prefab.GetComponent<RectTransform>().localPosition = position;
         rectTransform.localPosition = position;
+        WorldPosList[id] = GetWorldPositionFromRectPosition(rectTransform);
 
         // 引数に何番目のボタンかを渡す
         //int test = prefab.GetComponent<ButtonColorChange>().Id;  // 何故かこれでid取ってこれなかった、要検証
@@ -54,15 +60,13 @@ public class PointManeger : MonoBehaviour {
     void OnClickPoint(int Id)
     {
         // 点のつながりを更新
-        if(aheadPoint == -1)
-        {
-            aheadPoint = Id;
-        }
-        else
+        if(aheadPoint != -1)
         {
             connectionMap[Id, aheadPoint] = true;
             connectionMap[aheadPoint, Id] = true;
+            DrawLine(WorldPosList[aheadPoint], WorldPosList[Id]);
         }
+        aheadPoint = Id;
         /*
         //Debug.Log(connectionMap); // 型しか表示されねぇ！
         for(int i =0; i < pointNum; i++)
@@ -76,5 +80,30 @@ public class PointManeger : MonoBehaviour {
             }
         }
         */
+    }
+
+    void DrawLine(Vector2 start, Vector2 end)
+    {
+        var prefab = Instantiate(linePrefab, transform.position, transform.rotation) as GameObject;
+        prefab.GetComponent<line>().SetLine(start, end);
+    }
+
+    // uiの座標をworld座標に変換する関数
+    private Vector3 GetWorldPositionFromRectPosition(RectTransform rect)
+    {
+        /*
+        http://alien-program.hatenablog.com/entry/2017/08/06/164258
+        // こっからパクった
+        */
+        //UI座標からスクリーン座標に変換
+        Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, rect.position);
+
+        //ワールド座標
+        Vector3 result = Vector3.zero;
+
+        //スクリーン座標→ワールド座標に変換
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rect, screenPos, canvas.worldCamera, out result);
+        Debug.Log(screenPos);
+        return result;
     }
 }
---
+++ Assets/Script/line.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 無くてもいい気がするがサンプルプログラムにあったので書いとく
+[RequireComponent(typeof(LineRenderer))]
+public class line : MonoBehaviour {
+
+    //private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        /*
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
+        */
+    }
+
+    void Update()
+    {
+        // サンプルプログラム
+        /*
+        if (Input.GetMouseButtonDown(0))
+        {
+            // クリック位置した座標の取得。 
+            Vector2 goPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // 頂点を追加。 
+            ++this.lineRenderer.numPositions;
+
+            // 追加した頂点の座標を設定。 
+            this.lineRenderer.SetPosition(this.lineRenderer.numPositions - 1, goPosition);
+        }
+        */
+    }
+
+    public void SetLine(Vector2 start, Vector2 end)
+    {
+        // 幅とかの設定はいったんインスペクターでやることにする
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, start);
+        lineRenderer.SetPosition(1, end);
+    }
+}
