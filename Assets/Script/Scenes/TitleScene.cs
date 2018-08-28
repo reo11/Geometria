20f8bed5ebe8214eb44da53975d03a0c585bd6b9
ReoDesktop <reohirao116@gmail.com>
2018-08-29 05:30:59 +0900

    スタートボタン追加

---
+++ Assets/Script/Scenes/TitleScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScene : MonoBehaviour {
+	public void OnClickStart()
+    {
+        Debug.Log("start");
+		// buildに各シーンを追加しないといけない
+		SceneManager.LoadScene ("Ranking");
+    }
+}
