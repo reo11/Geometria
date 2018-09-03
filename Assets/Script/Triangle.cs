155896b4451d88e8b6b7e844b13178419fdf822d
hir0king <i.hiroki0307@icloud.com>
2018-09-03 16:11:53 +0900

    全ての三角形を描画できるように修正(時計回りに指定するように修正)

--- Assets/Script/Triangle.cs
+++ Assets/Script/Triangle.cs
@@ -21,18 +21,38 @@ public class Triangle : MonoBehaviour {
 		
 	}
 
-    public void SetTriangle(Vector3[] position)
+    public void SetTriangle(Vector3[] positions)
     {
+        CheckClockwise(positions);
         var mesh = new Mesh();
-        mesh.vertices = position;
-        mesh.triangles = new int[] {
-            0, 1, 2
-        };
+        mesh.vertices = positions;
+        if (CheckClockwise(positions))
+        {
+            mesh.triangles = new int[] {
+                0, 1, 2
+            };
+        }
+        else
+        {
+            mesh.triangles = new int[] {
+                2, 1, 0
+            };
+        }
         mesh.RecalculateNormals();
         var filter = GetComponent<MeshFilter>();
         filter.sharedMesh = mesh;
 
         var renderer = GetComponent<MeshRenderer>();
         renderer.material = _mat;
+    }
+
+    bool CheckClockwise(Vector3[] pos)
+    {
+        Vector3 side1 = pos[1] - pos[0];
+        Vector3 side2 = pos[2] - pos[0];
+
+        Vector3 ans = Vector3.Cross(side1, side2).normalized;
+        Debug.Log(ans[2] < 0);
+        return (ans[2] < 0);
     }
 }
