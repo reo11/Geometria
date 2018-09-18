df34e8eef3fdd6a6f8e2cbc9bf1f8fa64f24b2c7
hir0king <i.hiroki0307@icloud.com>
2018-09-18 20:17:07 +0900

    なんかcanvasの設定は同じはずなのにズレる

--- Assets/Script/PointManeger.cs
+++ Assets/Script/PointManeger.cs
@@ -8,6 +8,7 @@ public class PointManeger : MonoBehaviour
 {
 
     public GameObject pointPrefab;
+    public Canvas buttonCanvas;
     public Canvas canvas;
     public GameObject linePrefab;
     const int pointNum = 11;
@@ -66,7 +67,9 @@ public class PointManeger : MonoBehaviour
 
     void PointGen(Vector3 position, int id)
     {
-        var prefab = Instantiate(pointPrefab, canvas.transform, false) as GameObject;
+        var prefab = Instantiate(pointPrefab, buttonCanvas.transform, false) as GameObject;
+        // 親を設定する
+        prefab.transform.SetParent(buttonCanvas.transform, false);
         RectTransform rectTransform = prefab.GetComponent<RectTransform>();
         //prefab.GetComponent<RectTransform>().localPosition = position;
         rectTransform.localPosition = position;
@@ -150,6 +153,8 @@ public class PointManeger : MonoBehaviour
     void DrawLine(Vector2 start, Vector2 end)
     {
         var prefab = Instantiate(linePrefab, transform.position, transform.rotation) as GameObject;
+        // 親を設定
+        prefab.transform.SetParent(canvas.transform, false);
         prefab.GetComponent<line>().SetLine(start, end);
     }
 
@@ -242,6 +247,8 @@ public class PointManeger : MonoBehaviour
         };
 
         var prefab = Instantiate(trianglePrefab, transform.position, transform.rotation) as GameObject;
+        // 親を設定
+        prefab.transform.SetParent(canvas.transform, false);
         prefab.GetComponent<Triangle>().SetTriangle(position);
     }
 
